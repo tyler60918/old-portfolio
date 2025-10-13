e069005e5ca9ed12cae3851cdf0aad389ec084df
Tyler Pierce <ecreipt@gmail.com>
2025-10-13 00:58:15 -0600

    Added more projects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import ProjectCard from './ProjectCard';
 import capImg from './assets/capstone_logo.png';
 import githubLogo from './assets/github-mark.svg';
+import wamImg from './assets/wam.png';
+import terminalImg from './assets/terminal-logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Tyler's Portfolio</h1>
+        <h2>Tyler's Portfolio</h2>
         <a
           className="GitHub-link"
           href="https://github.com/tyler60918?tab=repositories"
@@ -19,7 +21,7 @@ function App() {
       </header>
       <div className="Project-area">
         <ProjectCard 
-          title="Capstone Project" 
+          title="Easy-Sync" 
           description="A web and desktop app for adding events from a file to a Google Calendar" 
           link="https://github.com/tyler60918/s25-syncineers" 
           image={capImg} 
@@ -27,7 +29,19 @@ function App() {
         <ProjectCard
           title="Wack-A-Mole"
           description="A simple wack-a-mole program that automatically wacks the mole using multi-threading."
-          link="https://github.com/tyler60918/cs452-hw4/"
+          link="https://github.com/tyler60918/cs452-hw4/tree/main/submission"
+          image={wamImg}
+        />
+        <ProjectCard
+          title="Shell Project"
+          description="A simple shell program that supports redirection, and background processes."
+          link="https://github.com/tyler60918/cs452-hw4/tree/main/submission"
+          image={terminalImg}
+        />
+        <ProjectCard
+          title="Thread-Safe Queue"
+          description="A thread-safe queue implementation using mutexes and condition variables."
+          link="https://github.com/tyler60918/Queue/tree/main"
           image=""
         />
       </div>
