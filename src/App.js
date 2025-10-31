66abc291a2ba1d08b37b6f13bf0fe8e23417691c
Tyler Pierce <ecreipt@gmail.com>
2025-10-31 10:50:52 -0600

    Fixed deployment issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import ContactPage from './components/ContactPage';
+import Footer from './components/Footer';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
