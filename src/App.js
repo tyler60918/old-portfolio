fd7e3a9aa18d3e727efc71e3bbfeb84ffd0a8ed2
Tyler Pierce <ecreipt@gmail.com>
2025-10-31 15:25:20 -0600

    Reverted to previous versions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Header from './components/Header';
 import Projects from './components/Projects';
-import ContactPage from './components/ContactPage';
 import Footer from './components/Footer';
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/react"
 import { ChakraProvider, createSystem, defaultSystem, defaultConfig } from '@chakra-ui/react';
 import { AlertProvider } from './context/alertContext';
-import Alert from './components/Alert';
 
 function App() {
   const sys = createSystem(defaultSystem, defaultConfig);
@@ -18,9 +16,7 @@ function App() {
         <main>
           <Header />
           <Projects />
-          <ContactPage />
           <Footer />
-          <Alert />
           <Analytics />
           <SpeedInsights />
         </main>
